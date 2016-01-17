016c5c4151da10ab73eb166ed7676a0bc4ebb09f
Jacob Morzinski <jmorzins@mit.edu>
2016-01-16 22:04:15 -0500

    ...

--- jdirstat/src/jdirstat/core.clj
+++ jdirstat/src/jdirstat/core.clj
@@ -5,9 +5,11 @@
                           FileStore
                           LinkOption
                           Path
-                          FileVisitor]
-           [java.lang Iterable]
-           (java.nio.file.attribute BasicFileAttributes))
+                          FileVisitor AccessMode]
+           (java.nio.file.attribute BasicFileAttributes
+                                    DosFileAttributes)
+           (java.nio.file.spi FileSystemProvider)
+           (sun.nio.fs WindowsFileAttributes))
   (:gen-class))
 
 (defn getFS
@@ -36,16 +38,71 @@
 ; Use (root-filestores) to get a list of roots
 ; Use FileStore/getFileStore(Path) to see what filestore a file is in
 
-(def ^FileStore ffs (first (root-filestores)))
-(doto ffs
-     .type
-     .name)
+(def fs (getFS))
 
 (-> fs
     (.getPath "C:" (into-array String ["Users" "Jacob" "vimfiles"]))
     .toFile
     .listFiles
     seq)
+
+
+(def lo-nofollow
+  "An array of LinkOption/NOFOLLOW_LINKS for certain java.nio functions."
+  (into-array LinkOption [(LinkOption/NOFOLLOW_LINKS)]))
+
+(def lo-empty
+  "An empty array of LinkOption for certain java.nio functions."
+  (into-array LinkOption []))
+
+(defn get-path
+  "Call (. fs (getPath) x xs...)"
+  ([fs x]
+   (.getPath fs x (into-array String [])))
+  ([fs x & xs]
+   (.getPath fs x (into-array String xs))))
+
+(def c (get-path fs "C:"))
+
+; Attribute views
+(def attribute-views (.supportedFileAttributeViews fs))
+; Result on Windows: => #{"owner" "dos" "acl" "basic" "user"}
+; Both "dos" and  "basic" have size information,
+; as well as "isRegularFile", "isOther", etc.
+
+;; For the "My Music" that was giving me errors,
+;; "isOther" is true (and I wonder what the "attributes" means)
+;; "attributes" appears to be a bitfield.
+;; Contains bits for isReadonly/hidden/system/ isDirectory / etc
+
+;; consider checking Files/isReadable
+;; or isAccessible(var0, new AccessMode[]{AccessMode.READ})
+
+(def attrs (Files/readAttributes c DosFileAttributes lo-empty))
+; => #<WindowsFileAttributes sun.nio.fs.WindowsFileAttributes@2776e813>
+
+(Files/readAttributes c "basic:*" lo-nofollow)
+
+(for [v attribute-views]
+  (Files/readAttributes c (str v ":*") lo-nofollow))
+
+(reduce into {}
+        (for [v attribute-views]
+          {v (Files/readAttributes c (str v ":*") lo-nofollow)}))
+
+(FileSystemProvider/installedProviders)
+
+(def provider (.provider fs))
+(def access-mode (into-array AccessMode [AccessMode/READ]))
+(.checkAccess provider c access-mode)
+;=>nil (success?)
+(.checkAccess provider my-music access-mode)
+;=> AccessDeniedException
+(.checkAccess provider dropbox access-mode)
+;=>NoSuchFileException
+; The Files/isReadable calls checkAccess in a try block
+; and monitors for any generic IO exception
+
 
 ; For this multi-arity function, Clojure guesses wrong.
 ; We have to be explicit that the second arg is an empty array.
