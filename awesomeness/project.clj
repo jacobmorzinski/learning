3cc7d3bb6a02729dc1c21405405c44e06fa8064c
Jacob Morzinski <jmorzins@mit.edu>
2016-05-27 19:04:18 -0400

    webscraping?

---
+++ awesomeness/project.clj
@@ -0,0 +1,9 @@
+(defproject awesomeness "0.1.0-SNAPSHOT"
+  :description "Dabbling with Clojure, Selenium-Webdriver, and PhantomJS"
+  :url "http://blog.zolotko.me/2012/12/clojure-selenium-webdriver-and-phantomjs.html"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :jvm-opts ["-Dphantomjs.binary.path=C:/phantomjs-1.9.8/phantomjs.exe"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-webdriver/clj-webdriver "0.6.0"]
+                 [com.github.detro.ghostdriver/phantomjsdriver "1.0.3"]])
---
+++ awesomeness/src/awesomeness/core.clj
@@ -0,0 +1,6 @@
+(ns awesomeness.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
