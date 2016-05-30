5b7e46bf64a15603e1f9bad54b6eac7c2af38df7
Jacob Morzinski <jmorzins@mit.edu>
2016-05-30 08:10:36 -0400

    merge changes

--- awesomeness/project.clj
+++ awesomeness/project.clj
@@ -3,6 +3,9 @@
   :url "http://blog.zolotko.me/2012/12/clojure-selenium-webdriver-and-phantomjs.html"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  ; download phantomjs drivers from http://phantomjs.org/download.html
+  ; other drivers information at
+  ; https://code.google.com/p/selenium/wiki/GettingStarted
   :jvm-opts ["-Dphantomjs.binary.path=C:/phantomjs-1.9.8/phantomjs.exe"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-webdriver/clj-webdriver "0.6.0"]
--- awesomeness/src/awesomeness/core.clj
+++ awesomeness/src/awesomeness/core.clj
@@ -4,3 +4,11 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+; (def mydriver (set-driver! {:browser :htmlunit}))
+; (.setJavascriptEnabled (:webdriver mydriver) true)
+
+; (take-screenshot :file "screenshot.png")
+; 
+; IllegalArgumentException No matching method found: getScreenshotAs for class org.openqa.selenium.htmlunit.HtmlUnitDriver
+;   clojure.lang.Reflector.invokeMatchingMethod (Reflector.java:53)
