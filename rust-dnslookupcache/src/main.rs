5a3c13bdc992bee5371ae64b42645bd04d186bc6
Jacob Morzinski <jmorzins@mit.edu>
2017-01-24 13:06:25 -0500

    Brief test of std::net::lookup_host

--- rust-dnslookupcache/src/main.rs
+++ rust-dnslookupcache/src/main.rs
@@ -1,3 +1,36 @@
+#![feature(lookup_host)]
+
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+use std::io::Result;
+use std::net::SocketAddr;
+
+#[derive(Debug)]
+struct DnsLookupCache {
+    cache: HashMap<String, Vec<SocketAddr>>,
+}
+
+impl DnsLookupCache {
+    fn new() -> DnsLookupCache {
+        DnsLookupCache {
+            cache: HashMap::new(),
+        }
+    }
+
+    fn lookup(&mut self, host: &str) -> Result<&Vec<SocketAddr>> {
+        match self.cache.entry(host.to_owned()) {
+            Entry::Occupied(e) => Ok(e.into_mut()),
+            Entry::Vacant(e)   => Ok(e.insert(do_lookup(host)?))
+        }
+    }
+}
+
+fn do_lookup(host: &str) -> Result<Vec<SocketAddr>> {
+    Ok(std::net::lookup_host(host)?.collect())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut dlc = DnsLookupCache::new();
+    {let res1 = dlc.lookup("mit.edu"); println!("{:?}", res1);}
+    {let res2 = dlc.lookup("mit.ed"); println!("{:?}", res2);}
 }
