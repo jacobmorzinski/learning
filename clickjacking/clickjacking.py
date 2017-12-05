3968fdefe672d5785a4ab9c9b1165c14c3f092a1
Jacob Morzinski <jmorzins@mit.edu>
2017-12-05 17:20:21 -0500

    demo of clickjacking with an overlay iframe

---
+++ clickjacking/clickjacking.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# https://gist.github.com/daimatz/3ca32ae11d9635372853
+# SimpleHTTPServer with custom headers
+
+# https://www.troyhunt.com/clickjack-attack-hidden-threat-right-in/
+# Clickjacking explained
+
+# https://www.owasp.org/index.php/Clickjacking_Defense_Cheat_Sheet
+# Clickjacking Defense Cheat Sheet
+
+
+import SimpleHTTPServer
+import BaseHTTPServer
+import sys
+
+"""
+Usage:
+    python httpd.py [port] [additional headers ...]
+Example:
+    python httpd.py 8000 'Pragma: no-cache' 'Cache-Control: no-cache' 'Expires: 0'
+"""
+
+class CustomHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_new_headers()
+
+        SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_new_headers(self):
+        for i in sys.argv[2:]:
+            key, value = i.split(":", 1)
+            self.send_header(key, value)
+
+if __name__ == '__main__':
+    BaseHTTPServer.test(HandlerClass=CustomHTTPRequestHandler, ServerClass = BaseHTTPServer.HTTPServer, protocol="HTTP/1.1")
